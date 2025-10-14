0479bb78843b2ca283477cca07528486b88e1164
Patrick Miziewicz <patrick.miziewicz@gmail.com>
2025-10-14 22:06:00 +1100

    Udpated line wrap behaviour when jumping words

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -33,6 +33,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        cpp = { 'clang_format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
